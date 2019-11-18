b9ea9c3b9bfdb3148a51ec52b2aa642d52e6caec
Ren <ren.hann@five.ai>
2019-11-18 12:01:59 +0000

    Adds hough ellipse algorithm

--- sobel/sobel.cpp
+++ sobel/sobel.cpp
@@ -26,6 +26,9 @@ Mat get_houghSpace(Mat &thresholdMag, Mat &gradientDirection, int width, int hei
 
 void collect_lines_from_houghSpace(Mat &houghSpace, std::vector<double> &rhoValues, std::vector<double> &thetaValues,
                                    double threshold);
+vector<tuple<Point, double, double, double>> houghEllipse(Mat &thresholdMag, int width, int height, double min_major, double min_minor, int detection_threshold);
+
+void drawEllipse(Mat &image, Mat thresholdedMag, vector<tuple<Point, double, double, double>> hough_ellipse);
 
 double calculate_houghSpace_voting_threshold(Mat &hough_space) {
     double max, min;
@@ -42,8 +45,6 @@ int main(int argc, const char **argv) {
     Mat image;
     image = imread(imgName, 1);
 
-    // namedWindow( "Original Image", CV_WINDOW_AUTOSIZE );
-    // imshow( "Original Image", image );
 
     cvtColor(image, image, CV_BGR2GRAY);
 
@@ -81,7 +82,50 @@ int main(int argc, const char **argv) {
 
     drawLines(image_clone, thresholdedMag, rho, theta);
 
+    double min_major = 70;
+    double min_minor = 50;
+    int detection_threshold = 100;
+
+    // vector<tuple<Point, double, double, double>> hough_ellipse = houghEllipse(thresholdedMag, image.cols, image.rows,
+    //     min_major, min_minor, detection_threshold);
+
+
+    drawEllipse(image_clone, thresholdedMag, hough_ellipse);
+
     return 0;
+}
+
+void drawEllipse(Mat &image, Mat thresholdedMag, 
+    vector<tuple<Point, double, double, double>> hough_ellipse) {
+
+    Mat ellipses(image.size(), image.type(), Scalar(0));
+
+    for (int i = 0; i < hough_ellipse.size(); i++) {
+
+        Point center = get<0>(hough_ellipse[i]);
+        double major_axis = get<1>(hough_ellipse[i]);
+        double minor_axis = get<2>(hough_ellipse[i]);
+        double alpha = get<3>(hough_ellipse[i]);
+        Size axes(major_axis, minor_axis);
+
+        ellipse(ellipses, center, axes, alpha, 0, 360,
+            Scalar(0, 0, 255),
+            2);
+        ellipse(image, center, axes, alpha, 0, 360,
+            Scalar(0, 0, 255),
+            2);
+    }
+
+    thresholdedMag.convertTo(thresholdedMag, CV_8U);
+
+    Mat overlay;
+    overlay.zeros(image.size(), image.type());
+    ellipses.copyTo(overlay, thresholdedMag);
+
+    imwrite("result/ellipses.jpg", ellipses);
+    imwrite("result/overlay.jpg", overlay);
+    imwrite("result/foundEllipses.jpg", image);
+
 }
 
 void drawLines(Mat &image, Mat thresholdedMag, std::vector<double> &rhoValues, std::vector<double> &thetaValues) {
@@ -127,8 +171,7 @@ void drawLines(Mat &image, Mat thresholdedMag, std::vector<double> &rhoValues, s
     imwrite("result/foundLines.jpg", image);
 }
 
-void collect_lines_from_houghSpace(Mat &houghSpace, std::vector<double> &rhoValues, std::vector<double> &thetaValues,
-                                   double threshold) {
+void collect_lines_from_houghSpace(Mat &houghSpace, std::vector<double> &rhoValues, std::vector<double> &thetaValues, double threshold) {
     /*
      * Populates the line vectors, and thresholds the houghspace.
      */
@@ -186,6 +229,96 @@ Mat get_houghSpace(Mat &thresholdMag, Mat &gradientDirection, int width, int hei
     normalize(hough_space, hough_space, 0, 255, NORM_MINMAX);
 
     return hough_space;
+}
+
+vector<tuple<Point, double, double, double>> houghEllipse(Mat &thresholdMag, int width, int height, double min_major, double min_minor, int detection_threshold) {
+    // Hough Ellipse detection based on 
+    // Xie, Yonghong, and Qiang Ji. "A new efficient ellipse detection method." Pattern Recognition, 2002. Proceedings. 16th International Conference on. Vol. 2. IEEE, 2002
+
+    // Find all edges coordinates by using the thresholded magnitude
+    vector<Point> locations;    
+    Mat mag = thresholdMag.clone();
+    mag.convertTo(mag, CV_8U);
+    findNonZero(mag, locations);
+
+    // Stores found ecllipse in (x0, y0, a, b, alpha) format
+    vector<tuple<Point, double, double, double>> ellipse;
+
+    // Iterate on all edge coordinates 
+    for (int m = 0; m < locations.size() - 2; m++) {
+        
+        // Get first pixel to lookup
+        int x1 = locations[m].x;
+        int y1 = locations[m].y;
+
+        for (int n = m + 1; n < locations.size() - 1; n++) {
+
+            // Get second pixel to lookup
+            int x2 = locations[n].x;
+            int y2 = locations[n].y;
+
+            double major_axis = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+            if (major_axis < min_major) continue;
+
+            // Get half-length of major axis (a) and orientation (alpha)
+            double a = major_axis/2;
+            double alpha;
+            double dy = y2 - y1;
+            double dx = x2 - x1;
+
+            if (dx != 0 && dy != 0) alpha = atan2(dy, dx);
+            else alpha = (double) atan(0);
+
+            // Calculate center of ellipse
+            int x0 = cvRound((x1 + x2)/2);
+            int y0 = cvRound((y1 + y2)/2);
+
+
+            Mat accumulator(1,
+                (int)(sqrt(pow(width,2) + pow(height,2)) + 1), 
+                CV_64F);
+
+            for (int o = n + 1; o < locations.size(); o++) {
+
+                // Get third pixel to lookup
+                int x = locations[o].x;
+                int y = locations[o].y;
+
+                double d = sqrt(pow(x - x0, 2) + pow(y - y0, 2));
+                if (d < min_minor) continue;
+                if (d > a) continue;
+
+                // Get half-length of minor-axis (b)
+                double f_square = pow(x - x2, 2) + pow(y - y2, 2);
+                double cos_tau_square = pow(
+                    (pow(a,2) + pow(d,2) - f_square)/(2*a*d)
+                    ,2);
+
+                // Assume b > 0 and avoid division by 0
+                double k = pow(a,2) - pow(d,2) * cos_tau_square;
+                if (k > 0 && cos_tau_square < 1) {
+                    int b = cvRound(sqrt((pow(a,2) * pow(d,2) * (1 - cos_tau_square))/k));
+                    accumulator.at<double>(0, b)++;
+                }
+                
+            }
+
+            double max;
+            Point maxLoc;
+
+            // Adds ellipse if the maximum of the accumulator exceeds threshold
+            minMaxLoc(accumulator, NULL, &max, NULL, &maxLoc);
+            if (max > detection_threshold) {
+                tuple<Point,double,double,double> params (Point(x0, y0), a, maxLoc.y, alpha);
+                ellipse.push_back(params);
+            }
+
+        }
+    }
+
+
+
+    return ellipse;
 }
 
 Mat convolution(Mat &input, int direction, Mat kernel, cv::Size image_size) {
