e3227440eab6728dc88d8a34c08c5321f520c7f2
Ren <ren.hann@five.ai>
2019-11-26 01:40:02 +0000

    added merging of very close bounding boxes

--- face.cpp
+++ face.cpp
@@ -17,8 +17,11 @@
 using namespace std;
 using namespace cv;
 
+#define MERGE_THRESHOLD 0.15
+
 /** Function Headers */
 void detectAndDisplay( Mat frame );
+vector<Rect> merge_faces(const vector<Rect> faces);
 
 /** Global variables */
 String cascade_name = "dartcascade/best.xml";
@@ -64,13 +67,117 @@ void detectAndDisplay( Mat frame )
 	// 2. Perform Viola-Jones Object Detection 
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
-       // 3. Print number of Faces found
-	std::cout << faces.size() << std::endl;
+	// Partitions bounding boxes
+	vector<Rect> partitioned_faces = merge_faces(faces);
+	
+	// Print out number of paritioned faces
+	cout << partitioned_faces.size() << endl;
+
 
        // 4. Draw box around faces found
-	for( int i = 0; i < faces.size(); i++ )
+	for( int i = 0; i < partitioned_faces.size(); i++ )
 	{
-		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
+		rectangle(frame, Point(partitioned_faces[i].x, partitioned_faces[i].y), Point(partitioned_faces[i].x + partitioned_faces[i].width, partitioned_faces[i].y + partitioned_faces[i].height), Scalar( 0, 255, 0 ), 2);
 	}
 
+}
+
+vector<Rect> merge_faces(const vector<Rect> faces) 
+{
+	// Partitions bounding boxes if IOU is above threshold
+	map<int, set<int>*> partitions;
+
+	// Iterate over each pair of faces
+	for( int i = 0; i < faces.size(); i++ )
+	{
+		map<int,set<int>*>::iterator i_it = partitions.find(i);
+		if (i_it == partitions.end()) 
+		{
+			// Insert default partitions
+			set<int>* initial_set = new set<int>();
+			initial_set->insert(i);
+			i_it = partitions.insert(pair<int,set<int>*>(i, initial_set)).first;
+		}
+
+		for (int j = i + 1; j < faces.size(); j++) 
+		{
+			// If IOU is above threshold, we partition them
+			Rect intersection = faces[i] & faces[j];
+			Rect box_union = faces[i] | faces[j];
+			float intersectionArea = intersection.area();
+			float unionArea = box_union.area();
+
+			if ((intersectionArea / unionArea) > MERGE_THRESHOLD) 
+			{	
+				map<int,set<int>*>::iterator j_it = partitions.find(j);
+				if (i_it != partitions.end() && j_it != partitions.end())
+				{
+					if (i_it->second != j_it->second) 
+					{
+						// Merge sets if pointers are of different partitions
+						i_it->second->insert(j_it->second->begin(), j_it->second->end());					
+						
+						set<int> temp = *j_it->second;
+
+						for (auto index : temp)
+						{	
+							// Change all pointers from partition of j to the new merged set
+							partitions[index] = i_it->second;
+						}
+
+					}
+
+				}
+				else if (i_it != partitions.end())
+				{
+					// Add j to partition i if j is not partitioned
+					i_it->second->insert(j);
+					partitions[j] = i_it->second;
+
+				}	
+				else if (j_it != partitions.end())
+				{
+					// Add i to partition j if i is not partitioned
+					j_it->second->insert(i);
+					partitions[i] = j_it->second;
+				}
+
+			}
+
+		}
+
+	}
+
+	vector<Rect> partitioned_faces;
+
+	for (auto elem : partitions) 
+	{
+		// Check if partition has been processed
+		int n = elem.second->size();
+		if (n > 0) {
+
+			// Find average position, width and height of Rect in partition	
+			Point pos = Point(0,0);
+			int width = 0, height = 0;
+
+			for (auto j : *elem.second) 
+			{
+				Rect face = faces[j];
+				pos += Point(face.x, face.y);
+				width += face.width;
+				height += face.height;
+			}
+			
+			pos = Point(cvRound(pos.x/n), cvRound(pos.y/n));
+			width = cvRound(width/n);
+			height = cvRound(height/n);
+			Rect avg_rect = Rect(pos, pos + Point(width, height));
+			
+			// Clears partition and push average rectangle
+			partitioned_faces.push_back(avg_rect);
+			elem.second->clear();
+		}	
+	}
+
+	return partitioned_faces;
 }
